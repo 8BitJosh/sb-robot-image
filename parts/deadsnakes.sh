7482ff5533f1db735f6c93f634d5a07db9778479
WillB97 <will@rudi.org.uk>
2023-07-22 20:50:48 +0100

    Install python 3.11

--- parts/deadsnakes.sh
+++ parts/deadsnakes.sh
@@ -6,7 +6,7 @@ gpg --keyserver keyserver.ubuntu.com --recv-keys F23C5A6CF475977595C89F51BA69323
 gpg --export F23C5A6CF475977595C89F51BA6932366A755776 > /usr/share/keyrings/ppa-deadsnakes.gpg
 
 # The focal version is selected because it was release slightly before bullseye, update to jammy for bookworm
-cat >>/etc/apt/sources.list.d/ppa-deadsnakes.list <<EOF 
+cat >>/etc/apt/sources.list.d/ppa-deadsnakes.list <<EOF
 deb [signed-by=/usr/share/keyrings/ppa-deadsnakes.gpg] http://ppa.launchpad.net/deadsnakes/ppa/ubuntu focal main
 EOF
 
@@ -14,8 +14,8 @@ EOF
 apt-get -y update
 
 # venv versions have the ensurepip library
-apt-get -y install python3.10-venv
-# setup pip using python 3.10
-python3.10 -m ensurepip --default-pip
+apt-get -y install python3.11-venv
+# setup pip using python 3.11
+python3.11 -m ensurepip --default-pip
 # make this the default python
-ln -sf /usr/bin/python3.10 /usr/bin/python3
+ln -sf /usr/bin/python3.11 /usr/bin/python3
